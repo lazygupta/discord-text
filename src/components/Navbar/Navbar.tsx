ae37a785b342ce3ede344c6a178819d86647de77
Ankit Gupta <guptashirley02@gmail.com>
2025-03-25 18:30:40 +0530

    fixed unused component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,31 +7,35 @@ import {
 } from '@mantine/core';
 import { Moon, Sun } from 'lucide-react';
 import classes from './HeaderMegaMenu.module.css';
-import { Text } from '@mantine/core';
 import { ColourfulText } from '../ui/colourful-text';
 
 export function HeaderMegaMenu() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-  const isDark = colorScheme === "dark";
-
+  const isDark = colorScheme === 'dark';
 
   return (
     <Box pb={80}>
       <header className={classes.header}>
-        <Group justify="space-between" h="100%">
-          <Text fw={700} size="xl">Discord <ColourfulText text="Colored" /> Text Generator</Text>
-            
-          <span className='flex justify-end m-4'>
-            <ActionIcon
-              onClick={toggleColorScheme}
-              size="lg"
-              variant="outline"
-              color={isDark ? "yellow" : "blue"}
-              style={{ borderRadius: "50%" }}
-            >
-              {isDark ? <Sun size={20} /> : <Moon size={20} />}
-            </ActionIcon>
-          </span>
+        <Group
+          justify="space-between"
+          align="center"
+          h="100%"
+          px="md"
+          className="rounded-xl shadow-lg"
+        >
+          <Title order={2} className="gradient-text" fw={800}>
+            <ColourfulText text="Discord Colored Text Generator" />
+          </Title>
+
+          <ActionIcon
+            onClick={toggleColorScheme}
+            size="lg"
+            variant="outline"
+            color={isDark ? 'yellow' : 'blue'}
+            style={{ borderRadius: '50%' }}
+          >
+            {isDark ? <Sun size={20} /> : <Moon size={20} />}
+          </ActionIcon>
         </Group>
       </header>
     </Box>
